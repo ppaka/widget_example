0f7b639f89b8546931867d37857be8be3276e4bc
ppaka <asj0216@gmail.com>
2021-06-06 01:17:42 +0900

    0606 end

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,21 @@ class WidgetApp extends StatefulWidget {
 }
 
 class _WidgetExampleState extends State<WidgetApp> {
+  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems = new List();
+  String _buttonText;
   String sum = '';
   TextEditingController value1 = TextEditingController();
   TextEditingController value2 = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    for(var item in _buttonList) {
+      _dropDownMenuItems.add(DropdownMenuItem(value: item, child: Text(item)));
+    }
+    _buttonText = _dropDownMenuItems[0].value;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,15 +70,32 @@ class _WidgetExampleState extends State<WidgetApp> {
                 padding: EdgeInsets.all(15),
                 child: RaisedButton(
                     child: Row(
-                      children: <Widget>[Icon(Icons.add), Text('더하기')],
+                      children: <Widget>[Icon(Icons.add), Text(_buttonText)],
                     ),
                     onPressed: () {
                       setState(() {
-                        int result = int.parse(value1.value.text) +
-                            int.parse(value2.value.text);
+                        var value1Int = double.parse(value1.value.text);
+                        var value2Int = double.parse(value2.value.text);
+                        var result;
+                        if (_buttonText == '더하기') {
+                          result = value1Int + value2Int;
+                        } else if (_buttonText == '빼기') {
+                          result = value1Int - value2Int;
+                        } else if (_buttonText == '곱하기') {
+                          result = value1Int * value2Int;
+                        } else {
+                          result = value1Int / value2Int;
+                        }
                         sum = '$result';
                       });
                     }),
+              ),
+              Padding(padding: EdgeInsets.all(15),
+                child: DropdownButton(items: _dropDownMenuItems, onChanged: (value) {
+                  setState(() {
+                    _buttonText = value;
+                  });
+                }, value: _buttonText,),
               )
             ],
           ),
